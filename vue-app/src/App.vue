3d3de3a0f1d0a870932a3d4e96be1c82a36de0d4
Sam Vredenburgh <sam.vburgh@gmail.com>
2017-04-17 14:27:11 -0500

    included bootstrap into app.vue

---
+++ vue-app/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+/* eslint-disable no-unused-vars */
+import jQuery from 'jquery'
+global.jQuery = jQuery
+var Bootstrap = require('bootstrap')
+
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+@import '../node_modules/bootstrap/dist/css/bootstrap.css';
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+</style>
---
+++ vue-app/src/components/Hello.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="hello container">
+    <h1>{{ msg }}</h1>
+    <div class="row">
+      <div class="col-sm-6">Column small 6</div>
+      <div class="col-sm-6">Column 2 small 6</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to DOSE Digital Assistant'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
