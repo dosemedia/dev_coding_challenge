c148eb092005a66748d94aa9b597be618d1af4c2
bradkoerner <b.koerner1@gmail.com>
2017-12-20 09:37:01 -0500

    Added poll results to Edit page

--- src/components/Admin/EditPoll.vue
+++ src/components/Admin/EditPoll.vue
@@ -1,19 +1,26 @@
 <template>
   <div>
-    <PollFields v-on:save="savePoll" :poll="poll"></PollFields>
+    <div>
+      <PollFields v-on:save="savePoll" :poll="poll"></PollFields>
+    </div>
+    <div>
+      <PollResults :poll="poll"></PollResults>
+    </div>
   </div>
 </template>
 
 <script>
 import db from '../../firebase.js'
 import PollFields from './PollFields'
+import PollResults from '../PollResults'
 
 let pollRef
 
 export default {
   name: 'EditPoll',
   components: {
-    PollFields
+    PollFields,
+    PollResults
   },
   created: function () {
     pollRef = db.ref('Polls/' + this.$route.params.id)
