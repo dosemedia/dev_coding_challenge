52e51f867bfb762f45ec2bd8930aa809ae76b614
James Lave <james@lave.cf>
2018-02-16 12:51:06 -0600

    Configure eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,14 @@
 module.exports = {
-    'extends': [
+    extends: [
         'plugin:vue/essential',
         'airbnb-base',
-    ]
+    ],
+    rules: {
+        'import/no-unresolved': 0,
+        'import/extensions': 0,
+        'no-console': 0,
+    },
+    globals: {
+        'firebase': true,
+    }
 };
