d9d2fe88836894ee045618abb3a76a340c067c17
James Lave <james@lave.cf>
2018-02-17 09:17:42 -0600

    Add informative alert upon auth error

--- src/components/AuthPage.vue
+++ src/components/AuthPage.vue
@@ -1,101 +1,104 @@
 <template>
-  <section class="hero is-primary is-fullheight">
-    <div class="hero-body">
-      <div class="container">
-        <div class="column is-4 is-offset-4">
-          <div class="box">
-            <div class="tabs is-centered is-fullwidth">
-              <ul>
-                <li :class="{'is-active': isActive === 'login'}"
-                    @click="onSelect('login')">
-                  <a class="has-text-grey">Log in</a>
-                </li>
-                <li :class="{'is-active': isActive === 'signup'}"
-                    @click="onSelect('signup')">
-                  <a class="has-text-grey">Sign up</a>
-                </li>
-              </ul>
+  <div>
+    <section class="hero is-primary is-fullheight">
+      <div class="hero-body">
+        <div class="container">
+          <div class="column is-4 is-offset-4">
+            <vue-alert></vue-alert>
+            <div class="box">
+              <div class="tabs is-centered is-fullwidth">
+                <ul>
+                  <li :class="{'is-active': isActive === 'login'}"
+                      @click="onSelect('login')">
+                    <a class="has-text-grey">Log in</a>
+                  </li>
+                  <li :class="{'is-active': isActive === 'signup'}"
+                      @click="onSelect('signup')">
+                    <a class="has-text-grey">Sign up</a>
+                  </li>
+                </ul>
+              </div>
+
+              <form v-if="isActive === 'login'">
+                <div class="field">
+                  <div class="control has-icons-left">
+                    <span class="icon is-small is-left">
+                      <i class="fas fa-envelope"></i>
+                    </span>
+                    <input class="input"
+                           type="email"
+                           placeholder="Your email"
+                           v-model="user.email">
+                  </div>
+                </div>
+
+                <div class="field">
+                  <div class="control has-icons-left">
+                    <span class="icon is-small is-left">
+                      <i class="fas fa-lock"></i>
+                    </span>
+                    <input class="input"
+                           type="password"
+                           placeholder="Your password"
+                           v-model="user.password">
+                  </div>
+                </div>
+                <button class="button is-primary is-fullwidth"
+                        @click.prevent="onLogin">
+                  Log in
+                </button>
+              </form>
+
+              <form v-if="isActive === 'signup'">
+                <div class="field">
+                  <div class="control has-icons-left">
+                    <span class="icon is-small is-left">
+                      <i class="fas fa-envelope"></i>
+                    </span>
+                    <input class="input"
+                           type="email"
+                           placeholder="Email"
+                           v-model="user.email">
+                  </div>
+                </div>
+                <div class="field">
+                  <div class="control has-icons-left">
+                    <span class="icon is-small is-left">
+                      <i class="fas fa-lock"></i>
+                    </span>
+                    <input class="input"
+                           type="password"
+                           placeholder="Password"
+                           v-model="user.password">
+                  </div>
+                </div>
+                <div class="field">
+                  <div class="control"
+                       :class="{'has-icons-left': passwordsMatch}">
+                    <span v-if="passwordsMatch"
+                          class="icon is-small is-left has-text-success">
+                      <i class="fas fa-check"></i>
+                    </span>
+                    <input class="input"
+                           :class="{'is-success': passwordsMatch}"
+                           type="password"
+                           placeholder="Confirm password"
+                           v-model="user.confirmationPassword">
+                  </div>
+                  <p v-if="passwordsMatch"
+                     class="help is-success">Passwords match</p>
+                </div>
+                <button class="button is-primary is-fullwidth"
+                        @click.prevent="onSignup">
+                  Sign up
+                </button>
+              </form>
             </div>
-
-            <form v-if="isActive === 'login'">
-              <div class="field">
-                <div class="control has-icons-left">
-                  <span class="icon is-small is-left">
-                    <i class="fas fa-envelope"></i>
-                  </span>
-                  <input class="input"
-                         type="email"
-                         placeholder="Your email"
-                         v-model="user.email">
-                </div>
-              </div>
-
-              <div class="field">
-                <div class="control has-icons-left">
-                  <span class="icon is-small is-left">
-                    <i class="fas fa-lock"></i>
-                  </span>
-                  <input class="input"
-                         type="password"
-                         placeholder="Your password"
-                         v-model="user.password">
-                </div>
-              </div>
-              <button class="button is-primary is-fullwidth"
-                      @click.prevent="onLogin">
-                Log in
-              </button>
-            </form>
-
-            <form v-if="isActive === 'signup'">
-              <div class="field">
-                <div class="control has-icons-left">
-                  <span class="icon is-small is-left">
-                    <i class="fas fa-envelope"></i>
-                  </span>
-                  <input class="input"
-                         type="email"
-                         placeholder="Email"
-                         v-model="user.email">
-                </div>
-              </div>
-              <div class="field">
-                <div class="control has-icons-left">
-                  <span class="icon is-small is-left">
-                    <i class="fas fa-lock"></i>
-                  </span>
-                  <input class="input"
-                         type="password"
-                         placeholder="Password"
-                         v-model="user.password">
-                </div>
-              </div>
-              <div class="field">
-                <div class="control"
-                     :class="{'has-icons-left': passwordsMatch}">
-                  <span v-if="passwordsMatch"
-                        class="icon is-small is-left has-text-success">
-                    <i class="fas fa-check"></i>
-                  </span>
-                  <input class="input"
-                         :class="{'is-success': passwordsMatch}"
-                         type="password"
-                         placeholder="Confirm password"
-                         v-model="user.confirmationPassword">
-                </div>
-                <p v-if="passwordsMatch"
-                   class="help is-success">Passwords match</p>
-              </div>
-              <button class="button is-primary is-fullwidth"
-                      @click.prevent="onSignup">
-                Sign up
-              </button>
-            </form>
           </div>
         </div>
       </div>
-    </div>
-  </section>
+    </section>
+  </div>
 </template>
 
 <script>
@@ -137,7 +140,11 @@ export default {
           .then(() => {
             this.$router.replace('/admin');
           })
-          .catch((error) => { console.error(error); });
+          .catch(({ message }) => {
+            this.$alert.danger({ message });
+          });
+      } else {
+        this.$alert.danger({ message: 'Passwords don\'t match' });
       }
     },
 
@@ -148,7 +155,9 @@ export default {
         .then(() => {
           this.$router.replace('/admin');
         })
-        .catch((error) => { console.error(error); });
+        .catch(({ message }) => {
+          this.$alert.danger({ message });
+        });
     },
   },
 };
@@ -157,6 +166,10 @@ export default {
 <style scoped>
 button {
   margin-top: 2rem;
+}
+
+.box {
+  margin-top: 0.5rem;
 }
 </style>
 
--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import VueAlert from '@vuejs-pt/vue-alert';
 import App from './App';
 import router from './router';
 
 Vue.config.productionTip = false;
+Vue.use(VueAlert);
 
 Vue.directive('focus', {
   // When the bound element is inserted into the DOM...
