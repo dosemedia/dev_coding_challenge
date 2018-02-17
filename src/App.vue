23dc4e4081d3faa2a581083f0287bf2e2519a536
James Lave <james@lave.cf>
2018-02-16 21:18:55 -0600

    Redirect to auth or admin page as necessary when loading app

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,15 @@
 
 <script>
 export default {
-
+  mounted() {
+    firebase
+      .auth()
+      .onAuthStateChanged((user) => {
+        if (user) {
+          return this.$router.replace('/admin');
+        }
+        return this.$router.replace('/auth');
+      });
+  },
 };
 </script>
