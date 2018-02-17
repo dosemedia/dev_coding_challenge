aa7ca8719b0545eef82e6f15e00d1e6467b3943b
James Lave <james@lave.cf>
2018-02-16 21:20:15 -0600

    Add PollList styling and logic

---
+++ src/components/PollList.vue
@@ -0,0 +1,40 @@
+<template>
+  <nav class="panel">
+    <a :key="index"
+       v-for="(poll, index) in polls"
+       @click="onSelectPoll(poll)"
+       class="panel-block is-active">
+      {{ poll.title }}
+    </a>
+    <div class="panel-block">
+      <a class="button is-primary is-outlined is-fullwidth">
+        <i class="fa fa-plus"
+           style="margin-right: 0.5rem;"></i>
+        Create new poll
+      </a>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  props: ['polls'],
+
+  methods: {
+    onSelectPoll(poll) {
+      this.$emit('pollselected', poll);
+    },
+
+    onCreatePoll() {
+
+    },
+  },
+};
+</script>
+
+<style>
+.poll-item {
+  cursor: default;
+}
+</style>
+
