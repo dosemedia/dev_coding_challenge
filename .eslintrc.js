1825f20b7059b0c89180c2bb7819baa759d5e59c
James Lave <james@lave.cf>
2018-02-16 19:17:42 -0600

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,9 +6,11 @@ module.exports = {
     rules: {
         'import/no-unresolved': 0,
         'import/extensions': 0,
+        'vue/no-dupe-keys': 0,
         'no-console': 0,
     },
     globals: {
         'firebase': true,
+        'db': true,
     }
 };
